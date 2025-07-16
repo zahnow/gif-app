188cb3a226b79095cce87fdd605ca8a81919511f
Nick Zahnow <2569561+zahnow@users.noreply.github.com>
2025-07-16 15:20:09 -0500

    updated frontend

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,95 +1,38 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import { Container, Heading, HStack, VStack, Text, Grid, Card, Image, Avatar, AvatarGroup, Spacer } from "@chakra-ui/react";
 
-export default function Home() {
+export default async function Home() {
+  const data = await fetch(`http://localhost:3001/api/gifs`);
+  const json = await data.json();
+
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <Container paddingTop={10}>
+      <HStack paddingBottom={5}>
+        <VStack alignItems={'start'}>
+          <Heading size={'2xl'}>GIF Finder</Heading>
+          <Text>Explore trending GIFs, search for your favorites, and more!</Text>
+        </VStack>
+        <Spacer />
+        <AvatarGroup>
+          <Avatar.Root>
+            <Avatar.Fallback/>
+            <Avatar.Image />
+          </Avatar.Root>
+        </AvatarGroup>
+      </HStack>
+      <Heading size={'lg'}>Trending GIFs</Heading>
+      <Grid templateColumns="repeat(auto-fill, minmax(200px, 1fr))" gap={6}>
+        {json.data?.map((gif: any) => (
+          <Card.Root overflow={'hidden'}
+            key={gif.id}
+          >
+              <Image src={gif.images.fixed_width.url} alt={gif.title} />
+            <Card.Body>
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+              <Text>{gif.title}</Text>
+            </Card.Body>
+          </Card.Root>
+        ))}
+      </Grid>
+    </Container>
   );
 }
